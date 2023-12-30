312cbb4a6efa2a43abc1b7e7676e892bcbf932a9
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-12-30 15:53:30 -0300

    Font and image

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <nav>
-    <router-link to="/" class="hover:underline hover:italic">Home</router-link>
+    <router-link to="/" class="hover:underline hover:italic flex flex-col items-center">
+      <img src="./assets/portrait.png" alt="Protrait" class="h-40 w-36" />
+      <div
+        class="text-center text-transparent bg-clip-text bg-gradient-to-r from-black to-blue-400"
+      >
+        <h1 class="text-7xl">Juan Cruz Stakys</h1>
+        <h2 class="text-4xl">- Frontend developer -</h2>
+      </div>
+    </router-link>
   </nav>
   <section class="text-center">
     <router-view @navigate="(link) => this.$router.push(link)" />
