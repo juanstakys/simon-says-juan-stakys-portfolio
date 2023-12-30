02dcd52a22fafdee893a69f1d944e0c889d3bcef
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-12-30 12:06:49 -0300

    First structure

---
+++ src/components/SimonPad.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <div>
+      <div>Projects</div>
+      <div>About</div>
+    </div>
+    <div>
+      <div>Contact</div>
+      <div>Blog</div>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
