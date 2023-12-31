03c6739042e5338679a0989c382ffcf5fabd2275
juanstakys <52844841+juanstakys@users.noreply.github.com>
2023-12-31 12:25:21 -0300

    About

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="flex flex-col items-center px-10 gap-6">
-    <h1 class="text-2xl">About me</h1>
+    <h1 class="text-3xl">About me</h1>
+    <hr />
+    <h2 class="text-2xl">Background</h2>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed dignissim erat.
-      Etiam imperdiet varius nunc ut laoreet. Aenean feugiat metus tortor, accumsan varius
-      dui fermentum ac. Praesent ac consequat nisi. Etiam auctor consectetur tincidunt.
-      Donec nec bibendum tellus, et vehicula ipsum. Aenean eget eros aliquet, consectetur
-      ex in, blandit sapien. Nunc faucibus, urna nec interdum suscipit, ex urna congue
-      lectus, vel bibendum tortor est et est. Cras placerat dolor arcu, in consequat
-      ligula consectetur vitae. Cras rutrum laoreet orci ut molestie. Vivamus vel eros
-      tincidunt est iaculis sodales et eget nisi. Sed tempus sagittis enim et maximus.
-      Nunc eget tortor feugiat, tempor libero ac, maximus ipsum. Donec egestas ante a nunc
-      auctor commodo.
+      I'm a passionate programmer from Argentina, with a fervor for MATHS and LANGUAGE
+      LEARNING. I've always been interested in technology and at 15 I started programming
+      in C++. I engaged in Maths Olympiads at around the same age, until I finished
+      secondary education. There, I discovered that I wanted to dedicate my life to
+      technology and engineering, driven by my curiosity about the world and how it works
+      and my enthusiasm for learning everyday and solving all kind of challenging
+      problems. I'm also very interested in science, specially physics and astronomy. I
+      enjoy facing problems with a logical perspective: that was always my greatest
+      strenght. In those years, I met several friends with the same interests as mine and
+      that helped me develop my skills and knowledge faster.
     </p>
+    <p>
+      Immediately after finishing secondary school, I started working as a Software
+      Developer at a consultant company in my country with clients all around Latin
+      America. Beyond science and technology, I have a humanistic side. I love learning
+      languages and I'm always looking for new ones to learn. I speak native Spanish and
+      fluent English. In my teens I've worked as a tutor for my classmates and people that
+      needed help for school. I have also developed a strong interest in linguistics and
+      philosophy. Nowadays I'm focused on my engineering career and becoming proficient in
+      English. I'm currently pursuing a degree in Systems Engineering at Universidad
+      Tecnológica Nacional. Driven by a passion for creating innovative projects, I
+      eagerly seek new and exciting opportunities to contribute my skills.
+    </p>
+    <h2 class="text-2xl">Work experience</h2>
+    <p>
+      As a teenager, I used to teach maths to my classmates and other kids in my city, and
+      help them with homework and preparing for tests. (continue...)
+    </p>
+    <h2 class="text-2xl">Education</h2>
     <p>
       Mauris sed velit nec purus aliquet pellentesque eget nec tellus. Donec elementum
       gravida ante vel tincidunt. Vivamus ut dapibus magna. Aenean iaculis lobortis massa
